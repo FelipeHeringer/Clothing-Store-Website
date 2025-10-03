584587bb3c6a0efdfb4228380cd687031caf53cb
Felipe Heringer <heringerfelipe14@gmail.com>
2025-10-03 15:26:41 -0300

    feat(user): add user entity class

---
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/entity/User.java
@@ -0,0 +1,36 @@
+package com.fhcs.clothing_store.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer userId;
+
+    @Column(name = "username", length = 150, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", length = 255, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha_hash", length = 255, nullable = false, unique = true)
+    private String passwordHash;
+
+}
