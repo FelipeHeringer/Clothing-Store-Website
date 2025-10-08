b3a5e63b93d4b0392e5c171a4dc3239729ee949a
Felipe Heringer <heringerfelipe14@gmail.com>
2025-10-08 19:14:19 -0300

    feat(user): add method to verify and hash password if changed
    - Implement erifyAndHashPasswordIfChanged in PasswordUtil
    - Check if a new password was provided in the patched user
    - Verify it differs from the existing password
    - Securely hash and update the password before persisting

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/util/PasswordUtil.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/util/PasswordUtil.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.fhcs.clothing_store.dto.UserPatchDto;
+import com.fhcs.clothing_store.entity.User;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -20,5 +23,15 @@ public class PasswordUtil {
 
     public boolean matchesPasswords(String rawPassword, String encodePassword) {
         return passwordEncoder.matches(rawPassword, encodePassword);
+    }
+
+    public void verifyAndHashPasswordIfChanged(User user, UserPatchDto userPatched) {
+        String newPassword = userPatched.getPasswordHash();
+        if (newPassword != null && !newPassword.isEmpty() &&
+                !matchesPasswords(newPassword, user.getPasswordHash())) {
+
+            String newHashedPassword = encodePassword(newPassword);
+            user.setPasswordHash(newHashedPassword);
+        }
     }
 }
