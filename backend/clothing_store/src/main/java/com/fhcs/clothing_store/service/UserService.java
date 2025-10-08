e5a96a438a7cb8a05015a40cad847439e5983654
Felipe Heringer <heringerfelipe14@gmail.com>
2025-10-08 19:06:21 -0300

    feat(user): implement methods to get and update user information
    This commit introduces a new method getUserInformation that retrieves user details from a JWT token.
    It also adds updateUserInformation, which applies changes to a user using JsonPatch. To support updates,
    two additional methods were implemented: one to verify if the new username or email already exist,
    ensuring uniqueness, and another to apply the patched fields to the User entity. Password updates are
    also handled by verifying and hashing the new password when changed.

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/UserService.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/UserService.java
@@ -3,10 +3,14 @@ package com.fhcs.clothing_store.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fhcs.clothing_store.dto.UserPatchDto;
 import com.fhcs.clothing_store.dto.request.RegisterRequest;
 import com.fhcs.clothing_store.entity.User;
 import com.fhcs.clothing_store.repository.UserRepository;
+import com.fhcs.clothing_store.util.JsonPatchUtil;
+import com.fhcs.clothing_store.util.JwtTokenUtil;
 import com.fhcs.clothing_store.util.PasswordUtil;
+import com.github.fge.jsonpatch.JsonPatch;
 
 import jakarta.validation.Valid;
 
@@ -17,7 +21,13 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
     private PasswordUtil passwordUtil;
+
+    @Autowired
+    private JsonPatchUtil jsonPatchUtil;
 
     public User createUser(@Valid RegisterRequest request) {
 
@@ -29,6 +39,75 @@ public class UserService {
         user.setPasswordHash(passwordHash);
 
         return userRepository.save(user);
+    }
+
+    public User getUserInformation(String token) throws Exception {
+
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("Token é obrigatório");
+        }
+
+        String username = jwtTokenUtil.extractUsername(token);
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new IllegalArgumentException("Usuário não encontrado");
+        }
+
+        return user;
+    }
+
+    public User updateUserInformation(String token, JsonPatch patch) throws Exception {
+
+        try {
+            if (token == null || token.isEmpty()) {
+                throw new IllegalArgumentException("Token é obrigatório");
+            }
+
+            String username = jwtTokenUtil.extractUsername(token);
+            User user = userRepository.findByUsername(username);
+
+            if (user == null) {
+                throw new IllegalArgumentException("Usuário não encontrado");
+            }
+
+            UserPatchDto userPatched = jsonPatchUtil.extractPatchedFields(patch, UserPatchDto.class);
+            applyChangesToUser(user, userPatched);
+
+            return userRepository.save(user);
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar informações do usuário: " + e.getMessage());
+        }
+    }
+
+    private void applyChangesToUser(User user, UserPatchDto userPatched) {
+        try {
+            verifyUsernameAndEmailUniqueness(user, userPatched);
+            if (userPatched.getUsername() != null && !userPatched.getUsername().isEmpty()) {
+                user.setUsername(userPatched.getUsername());
+            }
+            if (userPatched.getEmail() != null && !userPatched.getEmail().isEmpty()) {
+                user.setEmail(userPatched.getEmail());
+            }
+            passwordUtil.verifyAndHashPasswordIfChanged(user, userPatched);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar informações do usuário: " + e.getMessage());
+        }
+    }
+
+    private void verifyUsernameAndEmailUniqueness(User user, UserPatchDto userPatched) {
+        if (userPatched.getUsername() != null && !userPatched.getUsername().isEmpty()) {
+            if (exitsByUsername(userPatched.getUsername())) {
+                throw new IllegalArgumentException("Nome de usuário já está em uso");
+            }
+        }
+
+        if (userPatched.getEmail() != null && !userPatched.getEmail().isEmpty()) {
+            if (exitsByEmail(userPatched.getEmail())) {
+                throw new IllegalArgumentException("Email já está em uso");
+            }
+        }
     }
 
     public boolean exitsByUsername(String username) {
