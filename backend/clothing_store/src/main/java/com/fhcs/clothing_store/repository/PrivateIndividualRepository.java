8d57aaf534e96d81a5bff010c3a4efed6940b9e5
Felipe Heringer <heringerfelipe14@gmail.com>
2025-10-27 23:26:44 -0300

    feat: add private individual repository

---
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/repository/PrivateIndividualRepository.java
@@ -0,0 +1,10 @@
+package com.fhcs.clothing_store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fhcs.clothing_store.entity.PrivateIndividual;
+
+public interface PrivateIndividualRepository extends JpaRepository<PrivateIndividual, Integer> {
+    
+    
+}
