25342ff671b60bf1b95f4287844f9cd27a656467
Felipe Heringer <heringerfelipe14@gmail.com>
2025-10-29 22:57:35 -0300

    feat: implement methods to get and update the private individual information
    This commit introduces two main methods, the first getPrivateIndividualByToken, retrieve an individual's information from the access token provided to the user. The second method, updatePrivateIndividualInformation , applies changes to an individual using JsonPatch

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/PrivateIndividualService.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/PrivateIndividualService.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fhcs.clothing_store.dto.request.PrivateIndividualRequest;
+import com.fhcs.clothing_store.dto.PrivateIndividualPatchDto;
 import com.fhcs.clothing_store.entity.PrivateIndividual;
 import com.fhcs.clothing_store.entity.User;
 import com.fhcs.clothing_store.entity.address.Address;
 import com.fhcs.clothing_store.repository.PrivateIndividualRepository;
 import com.fhcs.clothing_store.repository.UserRepository;
+import com.fhcs.clothing_store.util.JsonPatchUtil;
 import com.fhcs.clothing_store.util.JwtTokenUtil;
+import com.github.fge.jsonpatch.JsonPatch;
 
 @Service
 public class PrivateIndividualService {
@@ -28,6 +31,9 @@ public class PrivateIndividualService {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JsonPatchUtil jsonPatchUtil;
 
     public PrivateIndividual createPrivateIndividual(String accessToken, PrivateIndividualRequest request) {
 
@@ -56,6 +62,55 @@ public class PrivateIndividualService {
         addressService.linkIndividualAndAddress(individual, address, request.getAddress().getDescription());
 
         return individual;
+    }
+
+    public PrivateIndividual getPrivateIndividualByToken(String accessToken) {
+        User user = userRepository.findByUsername(jwtTokenUtil.extractUsername(accessToken));
+        return individualRepository.findByUser_UserId(user.getUserId());
+    }
+
+    public PrivateIndividual updatePrivateIndividualInformation(String accessToken, JsonPatch patch) {
+
+        try {
+            PrivateIndividual privateIndividual = getPrivateIndividualByToken(accessToken);
+
+            PrivateIndividualPatchDto patchedIndividual = jsonPatchUtil.extractPatchedFields(patch,
+                    PrivateIndividualPatchDto.class);
+            applyChangesToIndividual(privateIndividual, patchedIndividual);
+
+            return individualRepository.save(privateIndividual);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar informações do indivíduo: " + e.getMessage());
+        }
+    }
+
+    private void applyChangesToIndividual(PrivateIndividual privateIndividual, PrivateIndividualPatchDto patchedIndividual) {
+
+        try {
+            verifyPhoneNumberUniqueness(patchedIndividual.getPhoneNumber(), privateIndividual.getIndividualId());
+            if (patchedIndividual.getIndividualName() != null) {
+                privateIndividual.setIndividualName(patchedIndividual.getIndividualName());
+            }
+            if (patchedIndividual.getPhoneNumber() != null) {
+                String formattedPhoneNumber = patchedIndividual.getPhoneNumber().replaceAll("[^\\d]", "");
+                privateIndividual.setPhoneNumber(formattedPhoneNumber);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao aplicar alterações no individuo: " + e.getMessage());
+        }
+    }
+
+    private void verifyPhoneNumberUniqueness(String phoneNumber, Integer individualId) {
+
+        String formattedPhoneNumber = phoneNumber.replaceAll("[^\\d]", "");
+
+        boolean exists = individualRepository.existsByPhoneNumberAndIndividualIdNot(formattedPhoneNumber, individualId);
+
+        if (exists) {
+            throw new IllegalArgumentException("O número de telefone já está em uso por outro indivíduo.");
+        }
+
     }
 
 }
