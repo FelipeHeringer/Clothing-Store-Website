7a388d1669578f34a959f9694be198d82b3d012e
Felipe Heringer <heringerfelipe14@gmail.com>
2025-11-12 14:17:25 -0300

    feat(address): add address management operations to AddressService
    This commit extends Address Service with methods to retreive, update and delete individual addresses.Supporting methods include applyChangesToAddress for selective field updates with CEP validation, applyChangesToIndividualAddress for updating address descriptions, and validateChangesWithViaCepResponse to verify that modified address data matches ViaCEP API responses. These methods work together to maintain address data integrity while supporting flexible updates through the JsonPatch pattern.

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/AddressService.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/service/AddressService.java
@@ -1,8 +1,11 @@
 package com.fhcs.clothing_store.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fhcs.clothing_store.dto.AddressPatchDto;
 import com.fhcs.clothing_store.dto.request.AddressRequest;
 import com.fhcs.clothing_store.dto.response.ViaCepResponse;
 import com.fhcs.clothing_store.entity.PrivateIndividual;
@@ -11,6 +14,7 @@ import com.fhcs.clothing_store.entity.address.CEP;
 import com.fhcs.clothing_store.entity.address.City;
 import com.fhcs.clothing_store.entity.address.IndividualAddress;
 import com.fhcs.clothing_store.entity.address.State;
+import com.fhcs.clothing_store.exception.AddressNotFoundException;
 import com.fhcs.clothing_store.exception.AddressValidationException;
 import com.fhcs.clothing_store.repository.address.AddressRepository;
 import com.fhcs.clothing_store.repository.address.CEPRepository;
@@ -44,7 +48,7 @@ public class AddressService {
         ViaCepResponse viaCepResponse = viaCepService.fetchAddressByCep(request.getCep());
 
         if (viaCepResponse.getError() != null) {
-            throw new RuntimeException("CEP não encontrado.");
+            throw new AddressNotFoundException("CEP não encontrado.");
         }
 
         validateAddress(request, viaCepResponse);
@@ -79,6 +83,106 @@ public class AddressService {
                 .build();
 
         return IndividualAddressRepository.save(individualAddress);
+    }
+
+    public List<IndividualAddress> getIndividualAddresses(Integer individualId) {
+        return addressRepository.findIndividualAddressesByIndividualId(individualId);
+    }
+
+    public Address updateAddressInformation(Address address, AddressPatchDto addressPatched) {
+
+        applyChangesToAddress(addressPatched, address);
+
+        return addressRepository.save(address);
+    }
+
+    public void deleteIndividualAddress(IndividualAddress individualAddress) {
+        IndividualAddressRepository.delete(individualAddress);
+    }
+
+    public void applyChangesToIndividualAddress(IndividualAddress individualAddress, AddressPatchDto addressPatched) {
+        if (addressPatched.getDescription() != null) {
+            individualAddress.setDescription(addressPatched.getDescription());
+            IndividualAddressRepository.save(individualAddress);
+        }
+    }
+
+    private void applyChangesToAddress(AddressPatchDto addressPatched, Address address) {
+
+        if (addressPatched.getCep() != null) {
+            String cleanCep = addressPatched.getCep().replace("-", "");
+            ViaCepResponse viaCepResponse = viaCepService.fetchAddressByCep(cleanCep);
+
+            if (viaCepResponse.getError() != null) {
+                throw new AddressNotFoundException("CEP não encontrado.");
+            }
+
+            try {
+                validateChangesWithViaCepResponse(addressPatched, viaCepResponse);
+                CEP cep = findOrCreateCEP(cleanCep);
+                address.setCep(cep);
+
+            } catch (Exception e) {
+                throw new RuntimeException("Erro ao aplicar mudanças ao endereço: " + e.getMessage());
+            }
+        }
+
+        if (addressPatched.getStreet() != null) {
+            address.setStreetName(addressPatched.getStreet());
+        }
+
+        if (addressPatched.getNumber() != null) {
+            address.setNumber(addressPatched.getNumber());
+        }
+
+        if (addressPatched.getCity() != null) {
+            City city = cityRepository.findByCityName(addressPatched.getCity());
+            address.setCity(city);
+        }
+
+        if (addressPatched.getState() != null) {
+            State state = stateRepository.findByUf(addressPatched.getState());
+            address.setState(state);
+        }
+
+        if (addressPatched.getComplement() != null) {
+            address.setComplement(addressPatched.getComplement());
+        }
+    }
+
+    private void validateChangesWithViaCepResponse(AddressPatchDto addressPatched,
+            ViaCepResponse viaCepResponse) {
+
+        StringBuilder errors = new StringBuilder();
+
+        if ((addressPatched.getStreet() != null &&
+                !addressPatched.getStreet().equalsIgnoreCase(viaCepResponse.getStreet()))
+                || addressPatched.getStreet() == null) {
+            errors.append("O nome da rua não corresponde ao CEP fornecido. Rua esperada: ")
+                    .append(viaCepResponse.getStreet())
+                    .append(". ");
+        }
+
+        if ((addressPatched.getCity() != null &&
+                !addressPatched.getCity().equalsIgnoreCase(viaCepResponse.getCity()))
+                || addressPatched.getCity() == null) {
+            errors.append("A cidade não corresponde ao CEP fornecido. Cidade esperada: ")
+                    .append(viaCepResponse.getCity())
+                    .append(". ");
+        }
+
+        if ((addressPatched.getState() != null &&
+                !addressPatched.getState().equalsIgnoreCase(viaCepResponse.getState()))
+                || addressPatched.getState() == null) {
+            errors.append("O estado não corresponde ao CEP fornecido. Estado esperado")
+                    .append(viaCepResponse.getState())
+                    .append(". ");
+        }
+
+        if (errors.length() > 0) {
+            throw new AddressValidationException("Falha na validação do Endereço: " + errors.toString());
+        }
+
     }
 
     private void validateAddress(AddressRequest request, ViaCepResponse viaCepResponse) {
