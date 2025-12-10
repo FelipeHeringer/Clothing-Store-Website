a82e0dbd74180a7a643d5766d67faf3119ff38ff
Felipe Heringer <heringerfelipe14@gmail.com>
2025-12-10 18:45:24 -0300

    feat(account): display AccountDetails if user is Authenticated

--- frontend/src/pages/AccountPage.tsx
+++ frontend/src/pages/AccountPage.tsx
@@ -1,17 +1,24 @@
+import { AccountDetails } from "@/components/AccountDetails";
 import { NavBar } from "@/components/NavBar";
 import { SignInForm } from "@/components/SignInForm";
 import { SignUpForm } from "@/components/SignUpForm";
+import { useAuth } from "@/contexts/AuthContext";
 import { useState } from "react";
 
 export function AccountPage() {
-    const  [showRegister, setShowRegister] = useState(false);
+    const [showRegister, setShowRegister] = useState(false);
+    const { isAuthenticated } = useAuth()
     return (
         <div className="flex flex-col bg-gray-100 min-h-screen items-center justify-center">
             <NavBar />
-            {showRegister ? (
-                <SignUpForm onSwitchToSignIn={() => setShowRegister(false)} />
+            {isAuthenticated ? (
+                <AccountDetails />
             ) : (
-                <SignInForm onSwitchToSignUp={() => setShowRegister(true)} />
+                showRegister ? (
+                    <SignUpForm onSwitchToSignIn={() => setShowRegister(false)} />
+                ) : (
+                    <SignInForm onSwitchToSignUp={() => setShowRegister(true)} />
+                )
             )}
         </div>
     );
