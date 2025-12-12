0c1e4cfa27d484244988d5434a4c09aa619bb460
Felipe Heringer <heringerfelipe14@gmail.com>
2025-12-11 21:53:22 -0300

    fix: fix incorrect annotation import

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/dto/request/product/CategoryRequest.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/dto/request/product/CategoryRequest.java
@@ -1,7 +1,6 @@
 package com.fhcs.clothing_store.dto.request.product;
 
-import javax.validation.constraints.NotBlank;
-
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
