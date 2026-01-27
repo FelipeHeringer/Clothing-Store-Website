6e5fa0c7bad94e96f1f07361cf7d76d78062de11
FelipeHeringer <heringerfelipe14@gmail.com>
2026-01-26 21:42:41 -0300

    feat(token): create RefreshTokenService and InvalidTokenException
    Create the Refresh Token Service with methods to create token in database and revoke tokens, and create a custom exception for invalid token

--- backend/clothing_store/src/main/java/com/fhcs/clothing_store/security/filter/JwtAuthenticationFilter.java
+++ backend/clothing_store/src/main/java/com/fhcs/clothing_store/security/filter/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String path = request.getRequestURI();
-        return path.equals("/api/auth/register") || path.equals("/api/auth/login") || path.equals("api/auth/refresh");
+        return path.equals("/api/auth/register") || path.equals("/api/auth/login") || path.equals("/api/auth/refresh");
     }
 
     @Override
